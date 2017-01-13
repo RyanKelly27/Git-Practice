5eacaa829909b2e45d0e360899c60a2d9b99a0c0
RyanKelly27 <ryankelly@Carleys-MacBook-Air.local>
2017-01-12 19:39:21 -0500

    Initial commit

---
+++ Yeezy/src/NoMorePartiesInLA.java
@@ -0,0 +1,12 @@
+
+public class NoMorePartiesInLA {
+
+	
+	
+	public static void main(String[] args)
+	{
+		System.out.println("I feel like Pablo when I'm working on my shoes.");
+		System.out.println("I feel like Pablo when I see me on the news.");
+		System.out.println("I feel like Pablo when I'm working on my house.");
+	}
+}
